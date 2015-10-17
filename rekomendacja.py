e1e2cc3a930b20657e3980c1b606e7abb993e882
AgataWa <4an321@gmail.com>
2015-10-17 17:59:02 +0200

    Zadanie 2

--- rekomendacja.py
+++ rekomendacja.py
@@ -1,48 +1,65 @@
-﻿#  W    zorowane na przykładzie Rona Zacharskiego
-
-
-from math import sqrt
-
-users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-         "Bonia":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
-         "Celina": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
-         "Dominika": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
-         "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
-         "Fruzia":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
-         "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
-        }
-
+﻿users = {
+    "Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5,
+             "The Strokes": 2.5, "Vampire Weekend": 2.0},
+    "Bonia": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5,
+              "Vampire Weekend": 3.0},
+    "Celina": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5,
+               "Slightly Stoopid": 1.0},
+    "Dominika": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5,
+                 "The Strokes": 4.0, "Vampire Weekend": 2.0},
+    "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+    "Fruzia": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5,
+               "The Strokes": 4.0, "Vampire Weekend": 4.0},
+    "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0,
+              "The Strokes": 5.0},
+    "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+}
 
 
 def manhattan(rating1, rating2):
-    """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
-       danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
-       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
-       
-    # TODO: wpisz kod
-    pass
+    distance = 0
+    compare = False
+    for k in rating1.keys():
+        if k in rating2.keys():
+            compare = True
+            distance += abs(rating1[k] - rating2[k])
+    if compare:
+        return distance
+    else:
+        return -1
 
 
 def computeNearestNeighbor(username, users):
-    """dla danego użytkownika username, zwróć ze słownika users nazwę użytkownika o najbliższych preferencjach"""
-    nameOfNearestNeighbor = ""
     distances = []
-    # TODO: wpisz kod
+    for i in users.keys():
+        if i != username:
+            dist = manhattan(users[username], users[i])
+            distances.append([dist, i])
+    distances.sort()
+    nameOfNearestNeighbor = distances[0][1]
     return nameOfNearestNeighbor
 
+
 def recommend(username, users):
-    """Zwróć listę rekomendacji dla użytkownika"""
-    # znajdź preferencje najbliższego sąsiada
     nearest = computeNearestNeighbor(username, users)
-    
     recommendations = []
-    # zarekomenduj użytkownikowi wykonawcę, którego jeszcze nie ocenił, a zrobił to jego najbliższy sąsiad
-    # TODO: wpisz kod
-    
-    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
+    for k in users[nearest].keys():
+        if k not in users[username].keys():
+            recommendations.append([k, users[nearest][k]])
+    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse=True)
 
-# przykłady
 
-print( recommend('Hela', users))
-#print( recommend('Celina', users))
+def test():
+    assert (manhattan(users["Ania"], users["Bonia"]) == 9.0)
+    assert (manhattan(users["Dominika"], users["Ela"]) == 4.5)
+    assert (computeNearestNeighbor("Dominika", users) == "Hela")
+    assert (computeNearestNeighbor("Gosia", users) == "Celina")
+    assert (recommend("Celina", users) == [['The Strokes', 4.0], ['Vampire Weekend', 1.0]])
+    assert (recommend("Fruzia", users) == [['Blues Traveler', 3.0]])
+
+
+test()
+
+
+print(recommend('Hela', users))
+print(recommend('Celina', users))
